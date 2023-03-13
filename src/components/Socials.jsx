3d4646c5f5b6d754a382a1e3fc63e9d0970bf4e7
ECarry <lianshiliang93@gmail.com>
2023-03-13 23:12:59 +0800

    feat: icons hover 及字体颜色修改

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,12 +5,12 @@ import { SiBilibili } from 'react-icons/si';
 
 const Socials = () => {
   return <div className='hidden xl:flex ml-24'>
-    <ul className='flex gap-x-4'>
-      <li><a href='https://www.bilibili.com' target='_blank'><SiBilibili /></a></li>
-      <li><a href='https://www.twitter.com' target='_blank'><ImTwitter /></a></li>
-      <li><a href='https://www.pinterest.com' target='_blank'><ImPinterest /></a></li>
-      <li><a href='https://www.instagram.com' target='_blank'><ImInstagram /></a></li>
-      <li><a href='https://www.weibo.com' target='_blank'><ImSinaWeibo /></a></li>
+    <ul className='flex gap-x-4 text-[#696c6d]'>
+      <li><a href='https://www.bilibili.com' target='_blank' className='hover:text-primary transition'><SiBilibili /></a></li>
+      <li><a href='https://www.twitter.com' target='_blank' className='hover:text-primary transition'><ImTwitter /></a></li>
+      <li><a href='https://www.pinterest.com' target='_blank' className='hover:text-primary transition'><ImPinterest /></a></li>
+      <li><a href='https://www.instagram.com' target='_blank' className='hover:text-primary transition'><ImInstagram /></a></li>
+      <li><a href='https://www.weibo.com' target='_blank' className='hover:text-primary transition'><ImSinaWeibo /></a></li>
     </ul>
   </div>;
 };
