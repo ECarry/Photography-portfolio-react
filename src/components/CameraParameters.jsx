829b5735d1a33453c3f38fbea685810ca1de21e8
ecarry <lianshiliang93@gmail.com>
2023-03-24 14:56:56 +0800

    使用 chatGPT 优化代码

--- src/components/CameraParameters.jsx
+++ src/components/CameraParameters.jsx
@@ -1,80 +1,61 @@
 import React from 'react';
+import DJI from '../assets/DJI.svg';
+import SONY from '../assets/SONY.png';
+import CANON from '../assets/CANON.png';
+import APPLE from '../assets/APPLE.png';
+import NIKON from '../assets/NIKON.png';
+import FUJIFILM from '../assets/FUJIFILM.png';
 
-import { RiStarFill, RiStarLine } from 'react-icons/ri';
+const brandLogos = {
+  Sony: SONY,
+  SONY: SONY,
+  DJI: DJI,
+  NIKON: NIKON,
+  Canon: CANON,
+  APPLE: APPLE,
+  FUJIFILM: FUJIFILM,
+};
 
-import DJI from '../assets/DJI.svg'
-import SONY from '../assets/SONY.png'
-import CANON from '../assets/CANON.png'
-import APPLE from '../assets/APPLE.png'
-import NIKON from '../assets/NIKON.png'
-import FUJIFILM from '../assets/FUJIFILM.png'
+const CameraParameters = ({ photo }) => {
+  const {
+    camera_brand,
+    camera_model,
+    camera_lens,
+    focal_length,
+    aperture,
+    shutter_speed,
+    iso,
+    altitude,
+  } = photo;
 
-
-const CameraParameters = (props) => {
-  const photo = props.photo
-
-  const fullStars = Math.floor(photo.rating);
-  const halfStar = photo.rating - fullStars >= 0.5;
-  const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
-
-  const stars = [];
-
-  for (let i = 0; i < fullStars; i++) {
-    stars.push(<RiStarFill key={`star-${i}`} />);
-  }
-
-  if (halfStar) {
-    stars.push(<RiStarFill key="star-half" half />);
-  }
-
-  for (let i = 0; i < emptyStars; i++) {
-    stars.push(<RiStarLine key={`empty-star-${i}`} />);
-  }
-
-
-  return <div className='flex justify-center w-full md:justify-between items-center md:px-10 text-[#696c6d]'>
-    {/* CAMERA TEXT */}
-    <div className='text-center hidden md:block'>
-      <span className='md:text-xl font-medium'>{photo.camera_brand} {photo.camera_model}</span>
-      <p className='hidden md:block'>{photo.camera_lens}</p>
-    </div>
-
-    {/* CAMERA PARAS */}
-    {photo.camera_brand && <div className='flex items-center gap-4'>
-      {/* CAMERA LOGO */}
-      <div className='flex justify-center items-center w-10 h-10'>
-        {(() => {
-          switch (photo.camera_brand) {
-            case 'Sony':
-              return <img src={SONY} alt="" />;
-            case 'SONY':
-              return <img src={SONY} alt="" />;
-            case 'DJI':
-              return <img src={DJI} alt="" />;
-            case 'NIKON':
-              return <img src={NIKON} alt="" />;
-            case 'Canon':
-              return <img src={CANON} alt="" />;
-            case 'APPLE':
-              return <img src={APPLE} alt="" />;
-            case 'FUJIFILM':
-              return <img src={FUJIFILM} alt="" />;
-            default:
-              return null;
-          }
-        })()}
+  return (
+    <div className="flex justify-center w-full md:justify-between items-center md:px-10 text-gray-600">
+      {/* CAMERA TEXT */}
+      <div className="text-center hidden md:block">
+        <span className="md:text-xl font-medium">{camera_brand} {camera_model}</span>
+        {camera_lens && <p>{camera_lens}</p>}
       </div>
 
-      {/* PARAS */}
-      <div className='font-medium flex gap-x-3'>
-        {photo.focal_length && <span>{photo.focal_length}mm</span>}
-        {photo.aperture && <span>f/{photo.aperture}</span>}
-        {photo.shutter_speed && <span>{photo.shutter_speed}s</span>}
-        {photo.iso && <span>ISO{photo.iso}</span>}
-      </div>
+      {/* CAMERA PARAS */}
+      {camera_brand && (
+        <div className="flex items-center gap-4">
+          {/* CAMERA LOGO */}
+          <div className="flex justify-center items-center w-10 h-10">
+            {brandLogos[camera_brand] && <img src={brandLogos[camera_brand]} alt="" />}
+          </div>
+
+          {/* PARAS */}
+          <div className="font-medium flex gap-x-3">
+            {focal_length && <span>{focal_length}mm</span>}
+            {aperture && <span>f/{aperture}</span>}
+            {shutter_speed && <span>{shutter_speed}s</span>}
+            {iso && <span>ISO{iso}</span>}
+            {altitude && <span>{altitude}m</span>}
+          </div>
+        </div>
+      )}
     </div>
-    }
-  </div>;
+  );
 };
 
 export default CameraParameters;
