c2e05cd6cfd0be5f246d9d93214d2ca009f11bd6
ecarry <lianshiliang93@gmail.com>
2023-03-30 15:14:54 +0800

    优化 portfolio 页面显示

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,10 +3,24 @@ import PhotoAlbum from "react-photo-album";
 import Lightbox from '../components/Lightbox';
 import getPhotos from '../api/photos';
 
-const renderPhoto = ({ imageProps: { alt, ...restImageProps } }) => (
-  <div>
-    <img alt={alt} style={{ width: "100%", height: "auto" }} {...restImageProps} />
-  </div>
+const renderPhoto = ({ imageProps: { alt, style, ...restImageProps } }) => (
+  <img 
+      alt={alt} 
+      style={{
+        ...style,
+        borderRadius: "10px",
+        overflow: 'hidden',
+        boxShadow: "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)",
+        transition: "transform 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+      }}
+      {...restImageProps}
+      onMouseEnter={(e) => {
+        e.target.style.transform = "scale(1.03)";
+      }}
+      onMouseLeave={(e) => {
+        e.target.style.transform = "scale(1)";
+      }}
+  />
 );
 
 const Portfolio = () => {
@@ -14,6 +28,16 @@ const Portfolio = () => {
   const [ photo, setPhoto] = useState([])
   const [ showLightbox, setShowLightbox ] = useState(false)
 
+  const handleOpenLightbox = () => {
+    document.body.style.overflowY = "hidden";
+    setShowLightbox(true);
+  };
+
+  const handleCloseLightbox = () => {
+    document.body.style.overflowY = "auto";
+    setShowLightbox(false);
+  };
+  
   useEffect(() => {
     const fetchMyData = async () => {
       const response = await getPhotos();
@@ -34,23 +58,26 @@ const Portfolio = () => {
         <h1 className='h1'>Portfolio</h1>
       </div>
       {/* IMAGES */}
-      <>
         <PhotoAlbum 
           photos={photos} 
           layout="masonry" 
+          columns={3}
+          padding={10}
+          spacing={20}
           renderPhoto={renderPhoto}
           onClick={(photos) => {
             setPhoto(photos.photo)
-            setShowLightbox(true)
+            handleOpenLightbox()
           }}
         />
-      </>
       {/* LIGHTBOX  */}
       {showLightbox && (
         <Lightbox
           image={photo.image}
           exif={photo.exif}
-          onClose={() => setShowLightbox(false)}
+          onClose={() => {
+            handleCloseLightbox()
+          }}
         />
       )}
     </div>
